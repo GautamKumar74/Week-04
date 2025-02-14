5ee7f0816b51eea84bde4dfa9730e317ccf6f9db
Gautam Kumar <gk62018@gmail.com>
2025-02-14 23:09:07 +0530

    [Gautam] Add . nested try catch solution added

---
+++ Day04/src/main/java/nestedtrycatchblock/NestedTryCatchExample.java
@@ -0,0 +1,33 @@
+package nestedtrycatchblock;
+
+import java.util.Scanner;
+
+public class NestedTryCatchExample {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int[] numbers= {10,20,30,40,50};
+
+        System.out.print("Enter the index: ");
+        int index = sc.nextInt();
+
+        System.out.print("Enter the divisor: ");
+        int divisor = sc.nextInt();
+
+        try {
+            int value= numbers[index];
+            try {
+                int result= value/divisor;
+                System.out.println("Result: " + result);
+            }
+            catch (ArithmeticException e){
+                System.out.println("Cannot divide by zero!");
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid array index!");
+        }
+        finally {
+            sc.close();
+        }
+    }
+}

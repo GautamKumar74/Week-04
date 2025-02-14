2578c48c067aea8b7ff5b31ecdbb597410ef9c39
Gautam Kumar <gk62018@gmail.com>
2025-02-14 16:53:54 +0530

    [Gautam] Add . try with resources solution added

---
+++ Day04/src/main/java/trywithresources/TryWithResourcesExample.java
@@ -0,0 +1,26 @@
+package trywithresources;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResourcesExample {
+    public static void main(String[] args) {
+
+        String fileName= "src/main/java/trywithresources/info.txt";
+
+        try(BufferedReader reader= new BufferedReader(new FileReader(fileName))){
+            String firstLine= reader.readLine();
+
+            if(firstLine!=null){
+                System.out.println("First line: " + firstLine);
+            }
+            else{
+                System.out.println("File is empty!");
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error reading file");
+        }
+    }
+}

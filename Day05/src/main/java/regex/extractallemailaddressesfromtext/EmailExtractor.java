ab0bcd1e646dfa6c60c1b90e383a0430db673034
Gautam Kumar <gk62018@gmail.com>
2025-02-15 13:43:48 +0530

    [Gautam] Add . extraction of all email addresses from text solution added

---
+++ Day05/src/main/java/regex/extractallemailaddressesfromtext/EmailExtractor.java
@@ -0,0 +1,34 @@
+package regex.extractallemailaddressesfromtext;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailExtractor {
+    public static void main(String[] args) {
+        String text= "Contact us at support@example.com and info@company.org";
+
+        List<String> emails= extractEmails(text);
+
+        for(String email: emails){
+            System.out.println(email);
+        }
+
+    }
+
+    public static List<String> extractEmails(String text){
+        List<String> emails= new ArrayList<>();
+
+        String emailRegex= "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
+
+        Pattern pattern= Pattern.compile(emailRegex);
+        Matcher matcher= pattern.matcher(text);
+
+        while (matcher.find()){
+            emails.add(matcher.group());
+        }
+
+        return emails;
+    }
+}

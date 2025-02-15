8c9439247636d4a5cc9e6f022ca446e2bcc30ce0
Gautam Kumar <gk62018@gmail.com>
2025-02-15 15:24:11 +0530

    [Gautam] Add . extract dates solution added

---
+++ Day05/src/main/java/regex/extractdates/ExtractDates.java
@@ -0,0 +1,30 @@
+package regex.extractdates;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractDates {
+    public static void main(String[] args) {
+        String text= "The events are scheduled for 12/05/2023, 15/08/2024, and 29/02/2020.";
+
+        List<String> dates= extractDates(text);
+        System.out.println(dates);
+    }
+
+    public static List<String> extractDates(String text){
+        List<String> dates= new ArrayList<>();
+
+        String regex= "\\b(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}\\b";
+
+        Pattern pattern= Pattern.compile(regex);
+        Matcher matcher= pattern.matcher(text);
+
+        while(matcher.find()){
+            dates.add(matcher.group());
+        }
+
+        return dates;
+    }
+}

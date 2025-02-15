2e98c0749681c36bb1a1b88397b15877e4037830
Gautam Kumar <gk62018@gmail.com>
2025-02-15 12:57:15 +0530

    [Gautam] Add . validate hex color code solution added

---
+++ Day05/src/main/java/regex/validatehexcolorcode/HexColorValidator.java
@@ -0,0 +1,16 @@
+package regex.validatehexcolorcode;
+
+public class HexColorValidator {
+
+    public static boolean isValidHexColor(String color){
+        return color.matches("^#[0-9A-Fa-f]{6}$");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValidHexColor("#FFA500"));
+        System.out.println(isValidHexColor("#ff4500"));
+        System.out.println(isValidHexColor("#123"));
+        System.out.println(isValidHexColor("FFA500"));
+        System.out.println(isValidHexColor("#GHIJKL"));
+    }
+}
